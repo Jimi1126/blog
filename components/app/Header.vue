172a95a38e5a93dd11ba7211574ea20542f349d6
jimi1126 <xfqing_mid@163.com>
2024-09-16 18:58:39 +0800

    feat: 更新Header组件和文章内容

--- components/app/Header.vue
+++ components/app/Header.vue
@@ -1,43 +1,41 @@
 <template>
-  <header
-    ref="headerRef"
-    class="flex items-center justify-between z-50"
-    :style="styles"
-  >
-    <UTooltip text="切换主题">
-      <UButton
-        :icon="
-          isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'
-        "
-        color="gray"
-        variant="ghost"
-        aria-label="Theme"
-        @click="isDark = !isDark"
-      />
-    </UTooltip>
+  <header ref="headerRef" class="flex items-center justify-between z-50" :style="styles">
     <UTooltip text="主页">
-      <UButton
-        icon="i-heroicons-home"
-        color="gray"
-        variant="ghost"
-        aria-label="Theme"
-        to="/"
-      />
+      <UButton icon="i-heroicons-home" color="gray" variant="ghost" aria-label="Theme" to="/" />
     </UTooltip>
+    <span>
+      <UTooltip text="切换主题">
+        <UButton
+          :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+          color="gray"
+          variant="ghost"
+          aria-label="Theme"
+          @click="isDark = !isDark" />
+      </UTooltip>
+      <UTooltip text="Github">
+        <UButton
+          icon="i-heroicons-face-smile"
+          color="gray"
+          variant="ghost"
+          aria-label="Theme"
+          target="_blank"
+          to="https://github.com/Jimi1126/blog" />
+      </UTooltip>
+    </span>
   </header>
 </template>
 <script lang="ts" setup>
-import { useFixedHeader } from 'vue-use-fixed-header';
+  import { useFixedHeader } from 'vue-use-fixed-header';
 
-const headerRef = ref(null);
-const { styles } = useFixedHeader(headerRef);
-const colorMode = useColorMode();
-const isDark = computed({
-  get() {
-    return colorMode.value === 'dark';
-  },
-  set() {
-    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark';
-  },
-});
+  const headerRef = ref(null);
+  const { styles } = useFixedHeader(headerRef);
+  const colorMode = useColorMode();
+  const isDark = computed({
+    get() {
+      return colorMode.value === 'dark';
+    },
+    set() {
+      colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark';
+    },
+  });
 </script>
